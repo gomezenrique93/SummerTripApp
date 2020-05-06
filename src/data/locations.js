7499e5f7ec1558a7391bc6fc8ec7aa535cf64360
Enrique Gomez <gomez.enrique4595@gmail.com>
2020-05-05 17:52:07 -0700

    First Commit

---
+++ src/data/locations.js
@@ -0,0 +1,134 @@
+
+
+
+export const locations = [
+  {
+    placename: 'Herndon, VA',
+    date: 'August 1, 2015',
+    location: {
+      lat: 38.958988,
+      lng: -77.417320
+    },
+    todo: [
+      'Where we start! 🚀'
+    ]
+  },
+  {
+    placename: 'Middlesboro, KY',
+    date: 'August 1, 2015',
+    location: {
+      lat: 36.627517,
+      lng: -83.621635
+    },
+    todo: [
+      'Cumberland Gap 🌳'
+    ]
+  },
+  {
+    placename: 'Nashville, TN',
+    date: 'August 2, 2015',
+    location: {
+      lat: 36.159708,
+      lng: -86.779786
+    },
+    todo: [
+      'Robert’s Western World Bar 🤠',
+      'Broadway Street 🎻',
+      'John Seigenthaler Pedestrian Bridge 🌉'
+    ]
+  },
+  {
+    placename: 'Lynchburg, TN',
+    date: 'August 3, 2015',
+    location: {
+      lat: 35.284334,
+      lng: -86.366818
+    },
+    todo: [
+      'Jack Daneils Distillery 🥃'
+    ]
+  },
+  {
+    placename: 'Huntsville, AL',
+    date: 'August 4, 2015',
+    location: {
+      lat: 34.711291,
+      lng: -86.653529
+    },
+    todo: [
+      'Sleep 😴',
+      'US Space and Rocket Center 👨‍🚀'
+    ]
+  },
+  {
+    placename: 'Atlanta, GA',
+    date: 'August 4, 2015',
+    location: {
+      lat: 33.746511,
+      lng: -84.397267
+    },
+    todo: [
+      'Flight ✈️'
+    ]
+  },
+  {
+    placename: 'Las Vegas, NV',
+    date: 'August 5, 2015',
+    location: {
+      lat: 36.171493,
+      lng: -115.138616
+    },
+    todo: [
+      'Casinos ♠♥♦♣',
+      'Walk the strip 💃',
+      'Fountain show ⛲'
+    ]
+  },
+  {
+    placename: 'Atlanta, GA',
+    date: 'August 7, 2015',
+    location: {
+      lat: 33.746511,
+      lng: -84.397267
+    },
+    todo: [
+      'Friends 🍻',
+      'Georgia Aquarium 🐠'
+    ]
+  },
+  {
+    placename: 'Columbia, SC',
+    date: 'August 8, 2015',
+    location: {
+      lat: 33.999298,
+      lng: -81.029584
+    },
+    todo: [
+      'Friends 🍻',
+      'Grill 🍖',
+      'Pool 🏊‍♂️'
+    ]
+  },
+  {
+    placename: 'Hamer, SC',
+    date: 'August 9, 2015',
+    location: {
+      lat: 34.498276,
+      lng: -79.309351
+    },
+    todo: [
+      'South of the Border 🧨'
+    ]
+  },
+  {
+    placename: 'Herndon, VA',
+    date: 'August 10, 2015',
+    location: {
+      lat: 38.958988,
+      lng: -77.417320
+    },
+    todo: [
+      'Where we end! 📍'
+    ]
+  }
+]; 
